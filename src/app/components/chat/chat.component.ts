1c18b66a6792c8b9832b60ca2231cc1cffb98664
Miquel Puig <mpuig22@gmail.com>
2019-06-21 14:59:25 +0200

    actulización mensajes

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -7,16 +7,26 @@ import { ChatService } from '../../providers/chat.service';
   templateUrl: './chat.component.html',
   styles: []
 })
-export class ChatComponent  {
+export class ChatComponent implements OnInit  {
   mensaje: string ="";
-
+  elemento: any;
   constructor(public _cs: ChatService) { 
 // this._cs.cargarMensajes().subscribe( (mensajes: any[])=>{
 //   console.log(mensajes);
 // })
 
-    this._cs.cargarMensajes().subscribe();
+    this._cs.cargarMensajes().subscribe( () =>{
 
+      setTimeout(()=>{
+        this.elemento.scrollTop = this.elemento.scrollHeight;
+      },20 )
+       
+    });
+
+  }
+
+  ngOnInit(){
+    this.elemento = document.getElementById('app-mensajes');
   }
   
   enviar_mensaje(){
